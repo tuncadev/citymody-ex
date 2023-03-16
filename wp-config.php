119fd5e6e0cfd649fadb1b8232ddbb8e1a4512c3
Ozgur M. Tunca <tunca.development@gmail.com>
2023-03-16 22:56:47 +0200

    untrack config

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'citymody_dev' );
 define( 'DB_PASSWORD', 'd)3)j3EE7v' );
 
 /** Database hostname */
-define( 'DB_HOST', 'citymody.mysql.tools' );
+ define( 'DB_HOST', 'citymody.mysql.tools' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
